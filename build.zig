3d0fbe4b825b497881e7fe9153d98b5473493ea2
Stanislav Silnicki <64929236+ssilnicki-dev@users.noreply.github.com>
2026-03-07 15:56:06 +0300

    build: add zig-driven meson build script

---
+++ build.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    _ = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const meson = b.option([]const u8, "meson", "Path to the meson executable") orelse "meson";
+    const ninja = b.option([]const u8, "ninja", "Path to the ninja executable") orelse "ninja";
+    const build_dir = b.option([]const u8, "build-dir", "Meson build directory") orelse "build-zig";
+    const prefix = b.option([]const u8, "prefix", "Install prefix") orelse "/usr/local";
+    const reconfigure = b.option(bool, "reconfigure", "Pass --reconfigure to meson setup") orelse false;
+
+    const buildtype = switch (optimize) {
+        .Debug => "debug",
+        .ReleaseSafe => "debugoptimized",
+        .ReleaseFast => "release",
+        .ReleaseSmall => "minsize",
+    };
+
+    const configure = b.addSystemCommand(&.{
+        meson,
+        "setup",
+        build_dir,
+        ".",
+        "--prefix",
+        prefix,
+        "--backend",
+        "ninja",
+        "--buildtype",
+        buildtype,
+        "--ninja",
+        ninja,
+    });
+    if (reconfigure) {
+        configure.addArg("--reconfigure");
+    }
+    configure.setEnvironmentVariable("CC", "zig cc");
+    configure.setEnvironmentVariable("CXX", "zig c++");
+    configure.setEnvironmentVariable("AR", "zig ar");
+    configure.setEnvironmentVariable("RANLIB", "zig ranlib");
+
+    const compile = b.addSystemCommand(&.{
+        meson,
+        "compile",
+        "-C",
+        build_dir,
+    });
+    compile.step.dependOn(&configure.step);
+
+    const install = b.addSystemCommand(&.{
+        meson,
+        "install",
+        "-C",
+        build_dir,
+    });
+    install.step.dependOn(&compile.step);
+
+    const configure_step = b.step("configure", "Configure DPDK with meson using zig toolchain");
+    configure_step.dependOn(&configure.step);
+
+    const compile_step = b.step("compile", "Compile DPDK");
+    compile_step.dependOn(&compile.step);
+
+    const install_step = b.step("install", "Install DPDK");
+    install_step.dependOn(&install.step);
+
+    b.default_step.dependOn(&compile.step);
+}
